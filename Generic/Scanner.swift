4964db443dcd71cd884dc952c4a53bfdee729411
Joost Heitbrink <joostheitbrink@outlook.com>
2016-03-02 09:25:08 +0100

    added macro to enable compiling on OSX as well as on Linux

--- Generic/Scanner.swift
+++ Generic/Scanner.swift
@@ -1,8 +1,12 @@
-import Glibc
+#if os(OSX)
+  import Darwin
+#else
+  import Glibc
+#endif
 import Swift
 
 public class FileScanner {
-	
+
 	//constants
 	private let NEW_LINE: Character = "\n"
 	private let EOF: Int32 = -1
@@ -17,17 +21,17 @@ public class FileScanner {
 	public func next(delimiter: Character) -> String {
 
 		var returnedString = String()
-		
+
 		while(true) {
-			
+
 			let readValue: Int32 = fgetc(file)
-			
+
 			if(readValue == EOF) {
 				return returnedString
 			}
 
 			let unsignedReadValue: UInt32 = UInt32(readValue)
-			let scalar: UnicodeScalar = UnicodeScalar(unsignedReadValue) 
+			let scalar: UnicodeScalar = UnicodeScalar(unsignedReadValue)
 			let char: Character = Character(scalar)
 
 			if(char == NEW_LINE || char == delimiter) {
@@ -35,7 +39,7 @@ public class FileScanner {
 			}
 
 			returnedString.append(char)
-		
+
 		}
 
 		return String()
